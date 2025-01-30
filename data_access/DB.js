ce31aa6ab8229c9f722d0e704bbbb105039a43cd
gsm1298 <gsm1298@rit.edu>
2025-01-30 17:57:07 -0500

    Implemented env file support, added DB constructor, and DB close method

--- data_access/DB.js
+++ data_access/DB.js
@@ -1,10 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 /**
  * @Class DB
  */
 export class DB {
-    /**
-     * @constructor
-     * @param tbd
-     */
-    constructor(){}
+    constructor(con = mysql) {
+        //change database credentials
+        this.con = mysql.createConnection({
+            host: process.env.host,
+            port: process.env.port,
+            user: process.env.user,
+            password: process.env.password,
+            database: process.env.database
+        });
+        this.con.connect(function (err) {
+            if (err) throw err;
+        });
+    }
+
+    close() {
+        this.con.end();
+    }
 }
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const server = express();
+
+dotenv.config()
 
 server.listen(3000, () => {
     console.log('Server Starting on http://localhost:3000');
