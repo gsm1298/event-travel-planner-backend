b2824f5ab0935cd9366dcf544d183283a34ec8ac
Nicholas Chrysanthopoulos <ngc8500@rit.edu>
2025-02-05 20:45:41 -0500

    Cookie fix/CORS

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 // Import service layer classes
 import { AuthService } from './service/AuthService.js';
 
 const server = express();
+
+server.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 dotenv.config();
 
